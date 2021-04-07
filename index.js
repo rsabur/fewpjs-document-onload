da565cf0d5fe075163ff58ccdd469c847dc456e9
Radiyah <rsabur58@gmail.com>
2021-04-07 08:29:27 -0400

    commit tree c8060d345257db0afaea4a256cd0eaab23e8ab6c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log("The DOM has loaded");
+    updateDOM()
+  });
+
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+
+function updateDOM() {
+    document.getElementById('text')
+        .innerHTML = "This is really cool!";
+}
